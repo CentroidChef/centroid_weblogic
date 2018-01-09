96e3465f5d529fdc92d780f76f78bc90c607f6d2
Kirt Martin <kirt.martin@centroid.com>
2018-01-09 16:26:00 -0500

    WebLogic Software Install Code Ready for Testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,21 +5,23 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # create the necessary groups and users
-group 'oinstall' do
-  gid '1002'
+group node['centroid_weblogic']['os_group'] do
   action :create
 end
 
-user 'oracle' do
-  gid 'oinstall'
-  password 'ZAQ!@WSX'
+user node['centroid_weblogic']['os_user'] do
+  comment 'Created by Chef for WebLogic installation'
+  gid node['centroid_weblogic']['os_group']
+  shell node['centroid_weblogic']['os_shell']
+  home node['centroid_weblogic']['os_user_home_dir'] + '/' + node['centroid_weblogic']['os_user']
+  manage_home true
   action :create
 end
 
 # create /stage folder for any installation files
 directory '/stage' do
-  owner 'oracle'
-  group 'oinstall'
+  owner 'root'
+  group 'root'
   mode '0755'
   recursive true
   action :create
@@ -28,8 +30,8 @@ end
 # get weblogic software from repository
 remote_file '/stage/fmw_12.2.1.3.0_wls.jar' do
   source 'http://chef-assets.centroid.com/software/weblogic/fmw_12.2.1.3.0_wls.jar'
-  owner 'oracle'
-  group 'oinstall'
+  owner 'root'
+  group 'root'
   mode '0755'
   action :create
 end
@@ -37,8 +39,8 @@ end
 # get java software from repository
 remote_file '/stage/jdk-8u151-linux-x64.tar.gz' do
   source 'http://chef-assets.centroid.com/software/java/jdk-8u151-linux-x64.tar.gz'
-  owner 'oracle'
-  group 'oinstall'
+  owner 'root'
+  group 'root'
   mode '0755'
   action :create
 end
@@ -69,20 +71,49 @@ execute 'chown java_home' do
   command 'chown -R root:root /java'
 end
 
-# add oraInst.loc to /etc for the oracle inventory location
-# template "#{params[:orainst_dir]}/oraInst.loc" do
-#   cookbook 'fmw_wls'
-#   source 'oraInst.loc'
-#   mode 0755
-#   variables(ora_inventory_dir: params[:ora_inventory_dir],
-#             os_group:          params[:os_group])
-#   action :create
-# end
-#
-# # create the oracle inventory location under the WebLogic OS user
-# directory params[:ora_inventory_dir] do
-#   owner params[:os_user]
-#   group params[:os_group]
-#   mode 0775
-#   action :create
-# end
+# create the oracle inventory location under the WebLogic OS user
+directory node['centroid_weblogic']['ora_inventory_dir'] do
+  owner node['centroid_weblogic']['os_user']
+  group node['centroid_weblogic']['os_group']
+  mode 0775
+  recursive true
+  action :create
+end
+
+# Add oraInst.loc to the oracle inventory location
+template "#{node['centroid_weblogic']['ora_inventory_dir']}/oraInst.loc" do
+  cookbook 'centroid_weblogic'
+  source 'oraInst.loc.erb'
+  mode 0755
+  variables(ora_inventory_dir: node['centroid_weblogic']['ora_inventory_dir'],
+            os_group: node['centroid_weblogic']['os_group'])
+  action :create
+end
+
+# create the middleware home directory
+directory node['centroid_weblogic']['middleware_home_dir'] do
+  owner node['centroid_weblogic']['os_user']
+  group node['centroid_weblogic']['os_group']
+  mode 0775
+  recursive true
+  action :create
+end
+
+# Setup the Weblogic 12c response file for the silent install
+template "#{node['centroid_weblogic']['tmp_dir']}/wls_12c.rsp" do
+    cookbook 'centroid_weblogic'
+    source 'wls_12c.rsp.erb'
+    mode 0755
+    owner node['centroid_weblogic']['os_user']
+    group node['centroid_weblogic']['os_group']
+    variables(middleware_home_dir: node['centroid_weblogic']['middleware_home_dir'],
+              install_type: node['centroid_weblogic']['install_type'])
+end
+
+# Run the installation
+execute 'Install WLS' do
+  command "#{node['centroid_weblogic']['java_home']}/bin/java -Xmx1024m -Djava.io.tmpdir=#{node['centroid_weblogic']['tmp_dir']} -jar /stage/#{node['centroid_weblogic']['wls_source_filename']} -silent -responseFile #{node['centroid_weblogic']['tmp_dir']}/wls_12c.rsp -invPtrLoc #{node['centroid_weblogic']['ora_inventory_dir']}/oraInst.loc"
+  user node['centroid_weblogic']['os_user']
+  group node['centroid_weblogic']['os_group']
+  cwd node['centroid_weblogic']['tmp_dir']
+end
