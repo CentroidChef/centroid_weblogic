82cd8c0a5d171592ac18b1f33db86362804b5112
Kirt Martin <kirt.martin@centroid.com>
2018-01-18 15:50:24 -0500

    Added default_domain start

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,24 +8,16 @@ require 'spec_helper'
 
 describe 'centroid_weblogic::default' do
 
-  context 'When WebLogic is running' do
-    stub_command("ps -ef | grep startWebLogic.sh | grep default_domain").and_return(1)
-  end
-
-  context 'When WebLogic is NOT running' do
-    stub_command("ps -ef | grep startWebLogic.sh | grep default_domain").and_return(0)
-  end
-  
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When all attributes are default, on an Oracle 7 platform' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'oracle', version: '7.4')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      chef_run # expect { chef_run }.to_not raise_error
     end
   end
 end
