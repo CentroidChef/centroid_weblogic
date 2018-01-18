ccd13fc091b4d8d67bc09fa4d5a34c12f8ab56f8
Kirt Martin <kirt.martin@centroid.com>
2018-01-18 16:07:28 -0500

    Added default_domain start

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,17 +7,30 @@
 require 'spec_helper'
 
 describe 'centroid_weblogic::default' do
+  let(:chef_run) do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+    runner = ChefSpec::ServerRunner.new(platform: 'oracle', version: '7.4')
+    runner.converge(described_recipe)
+  end
 
-  context 'When all attributes are default, on an Oracle 7 platform' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'oracle', version: '7.4')
-      runner.converge(described_recipe)
-    end
+  context 'WebLogic default_domain is running' do
+    it 'Skips Command' do
+      stub_command("ps -ef | grep startWebLogic.sh | grep default_domain").and_return(0)
 
-    it 'converges successfully' do
-      chef_run # expect { chef_run }.to_not raise_error
+      expect { chef_run }.to run_bash('Start WebLogic Domain')
     end
   end
+
+  context 'WebLogic default_domain is NOT running' do
+    it 'Runs Command' do
+      stub_command("ps -ef | grep startWebLogic.sh | grep default_domain").and_return(0)
+
+      expect { chef_run }.not_to run_bash('Start WebLogic Domain')
+    end
+  end
+
+  # it 'converges successfully' do
+  #   chef_run # expect { chef_run }.to_not raise_error
+  # end
 end
